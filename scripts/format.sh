465ed02e19a13485fb7121c9d99a38cc828bf3d4
Cameron <c.bryce996@gmail.com>
2025-12-27 12:39:18 +0000

    Removing lint and coverage from github workflows for now

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,7 +2,6 @@
 set -e
 
 echo "=== Checking code format ==="
-# Exits with error if any files are not correctly formatted
 FILES=$(find src tests -regex '.*\.\(cpp\|h\)$')
 clang-format -style=file -output-replacements-xml $FILES | grep "<replacement " && \
     echo "❌ Formatting issues found" && exit 1 || echo "✅ Format OK"
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,5 +2,4 @@
 set -e
 
 echo "=== Running clang-tidy lint ==="
-# Use -p build to get compile commands
 clang-tidy src/**/*.cpp -p build
