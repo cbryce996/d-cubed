cdab01dc43e70ed90480a6d519466f013014a16f
Cameron <c.bryce996@gmail.com>
2025-12-27 12:32:02 +0000

    Updating test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "=== Configuring CMake ==="
-cmake -B build -S . -G Ninja -DCMAKE_BUILD_TYPE=Debug
-
-echo "=== Building game and tests ==="
-cmake --build build
+echo "=== Running unit tests ==="
+ctest --test-dir build --output-on-failure
